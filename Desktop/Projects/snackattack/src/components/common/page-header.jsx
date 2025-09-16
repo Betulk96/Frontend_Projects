9577186245a354edc03ba2f05e2991626a0e7a9c
Betül Kuyucu <142298887+Betulk96@users.noreply.github.com>
2025-09-16 17:19:52 +0300

    Delete Desktop directory

--- Desktop/Projects/snackattack/src/components/common/page-header.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-const PageHeader = ({ children }) => {
-	return <h1 className="items-center">{children}</h1>;
-};
-
-export default PageHeader;
--- Desktop/Projects/snackattack/src/components/common/spacer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Spacer = ({ height = 75 }) => {
-	return <div style={{ height: `${height}px` }}></div>;
-};
-
-export default Spacer;
